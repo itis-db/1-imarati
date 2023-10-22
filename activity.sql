e227a70c736752f4f9ead4f9fcb2c29dfa685a16
imarati <iimarioii3@mail.ru>
2023-10-22 18:57:22 +0300

    add control work

--- activity.sql
+++ activity.sql
@@ -1,15 +1,14 @@
--- рекурсия 
-WITH RECURSIVE r AS (
-    SELECT 
-        1 AS i, 
-        1 AS factorial
-    
-    UNION 
-
-    SELECT 
-        i+1 AS i, 
-        factorial * (i+1) as factorial 
-    FROM r
-    WHERE i < 10
+WITH RECURSIVE EntityHierarchy AS (
+  SELECT P.ActivityId, A.ActivityTypeId, A.Name, 1 AS Level
+  FROM Program P
+  INNER JOIN Activity A ON P.ActivityId = A.ActivityId
+  
+  UNION ALL
+  
+  SELECT A.ActivityId, A.ActivityTypeId, A.Name, EH.Level + 1
+  FROM Activity A
+  INNER JOIN EntityHierarchy EH ON A.ParentId = EH.ActivityId
 )
-SELECT * FROM r;
+SELECT *
+FROM EntityHierarchy
+ORDER BY Level, ActivityId;
